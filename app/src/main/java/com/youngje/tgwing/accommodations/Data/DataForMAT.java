a843cdbbf557edbf200b9b35cefc66b68d701115
young <siosio34@nate.com>
2016-09-26 17:34:15 +0900

    first open project

---
+++ app/src/main/java/com/youngje/tgwing/accommodations/Data/DataForMAT.java
@@ -0,0 +1,7 @@
+package com.youngje.tgwing.accommodations.Data;
+
+/**
+ * Created by joyeongje on 2016. 9. 26..
+ */
+public class DataForMAT {
+}
---
+++ app/src/main/java/com/youngje/tgwing/accommodations/Data/Json.java
@@ -0,0 +1,9 @@
+package com.youngje.tgwing.accommodations.Data;
+
+/**
+ * Created by joyeongje on 2016. 9. 26..
+ */
+public class Json {
+
+
+}
