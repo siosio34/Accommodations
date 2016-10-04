6595b058fb0b4d246fd7d159e3315332bd4c8e39
young <siosio34@nate.com>
2016-10-04 21:33:18 +0900

    add config file

--- app/src/main/java/com/youngje/tgwing/accommodations/Data/DataForMAT.java
+++ app/src/main/java/com/youngje/tgwing/accommodations/Data/DataForMAT.java
@@ -2,14 +2,22 @@ package com.youngje.tgwing.accommodations.Data;
 
 import android.content.res.Resources;
 
+import java.util.Locale;
+
 /**
  * Created by joyeongje on 2016. 9. 26..
  */
 public class DataFormat {
 
+    private final String TOUR_KEY = "6P%2BoZASTR%2FyMvmypmG5GiuDxZywpTc43N4KDgbWDU9nzKaYCi%2Bx%2BQAMUBIKS7s%2BOML3LSCUkENhFVKV4KY3%2FQg%3D%3D";
+
+    //private final String TOUR_AMERICA   = "6P%2BoZASTR%2FyMvmypmG5GiuDxZywpTc43N4KDgbWDU9nzKaYCi%2Bx%2BQAMUBIKS7s%2BOML3LSCUkENhFVKV4KY3%2FQg%3D%3D";
+
+    // TODO: 2016. 10. 2. apikey 다 같은지 비교해야됨
+
 
     public enum DATASOURCE {
-        DAUM, OPENAPI,TOURAPI
+        DAUM, OPENAPI, TOURAPI
     }
 
     // -- 다음거
@@ -31,12 +39,13 @@ public class DataFormat {
     // HP8 : 병원
     // PM9 : 약국
 
-    public enum DATAFORMAT {
-        MART("MT1"), CONVINEIENCE("CS2"), KIDHOUSE("PS3"), SCHOOL("SC4"), INSTITUDE("AC5"),PARKING("PK6"), OIL("OL7"), SUBWAY("SW8"), BANK("BK9"), CUSTOM("CT1"), AG2("AG2"),PO3("PO3"),AT4("AT4"),MOTEL("AD5"),FOOD("FD6"),CAFE("CE7"),HOSPITAL("HP8"),PHARMACY("PM9");
+    public enum DAUMECATEGORY {
+
+        MART("MT1"), CONVINEIENCE("CS2"), KIDHOUSE("PS3"), SCHOOL("SC4"), INSTITUDE("AC5"), PARKING("PK6"), OIL("OL7"), SUBWAY("SW8"), BANK("BK9"), CUSTOM("CT1"), AG2("AG2"), PO3("PO3"), AT4("AT4"), MOTEL("AD5"), FOOD("FD6"), CAFE("CE7"), HOSPITAL("HP8"), PHARMACY("PM9");
 
         private final String value;
 
-        private DATAFORMAT(String value) {
+        private DAUMECATEGORY(String value) {
             this.value = value;
         }
 
@@ -47,50 +56,74 @@ public class DataFormat {
 
     //public static String[] DATSAFORMATNAME = {, "CS2", "PS3", "SC4", "AC5", "PK6", "OL7", "SW8", "BK9", "CT1", "AG2", "PO3", "AT4", "AD5", "FD6", "CE7", "HP8", "PM9"};
 
-    public static String createDaumRequestURL(DATASOURCE datasource,DATAFORMAT dataformat, double lat,double lon, int radius,int sort,String format,String daumApikey) { // 카테고리
-        String requestUrl  = "";
-        String APIkey = daumApikey;
+    public static String createDaumCategoryRequestURL(DAUMECATEGORY dataformat, double lat, double lon, int radius, int sort, String format, String daumApikey) { // 카테고리
+        String requestUrl = "";
         String searchType = dataformat.getValue(); // 뭘 검색해야하나
-        String curloc =  Double.toString(lat) +"," + Double.toString(lon);
-
-        // TODO: 2016. 9. 26.
-        // getString 함수는 컨텍스트나 액티비티에서 받아야된다
+        String curloc = Double.toString(lat) + "," + Double.toString(lon);
 
         // TODO: 2016. 9. 27. 이거는 다시짤가 생각좀
 
-        switch (datasource) {
-            case DAUM:
-                requestUrl = "https://apis.daum.net/local/v1/search/category.json?" +
-                        "apikey=" + APIkey + "&code=" + searchType +"&location=" + curloc +
-                        "&radius=" + radius + "&sort=" + sort;
+        requestUrl = "https://apis.daum.net/local/v1/search/category.json?" +
+                "apikey=" + daumApikey + "&code=" + searchType + "&location=" + curloc +
+                "&radius=" + radius + "&sort=" + sort;
 
-                // sort 0 정확도 1 인기 2 거리
-                // xml 요청도 가능한데 그건 나중에 ㅇㅇ
-                break;
-            case OPENAPI:
-                break;
+        // sort 0 정확도 1 인기 2 거리
+        // xml 요청도 가능한데 그건 나중에 ㅇㅇ
 
-            case TOURAPI:
-                break;
-
-        }
         return requestUrl;
 
         //ex https://apis.daum.net/local/v1/search/category.json?apikey={apikey}&code=PM9&location=37.514322572335935,127.06283102249932&radius=20000
         // 서울 강남구 삼성동 20km 반경에서 약국을 찾고 json 받기
     }
 
-    public static String createOpenAPIRequestURL(DATASOURCE datasource,DATAFORMAT dataformat, double lat,double lon, int radius,int sort,String format,String daumApikey) {
+    public static String createDaumKeywordRequestURL(String searchString ,double lat, double lon, int radius,int sort,String format,String daumApikey) {
+        String requestUrl = "";
+        String curloc = Double.toString(lat) + "," + Double.toString(lon);
+
+        requestUrl = "https://apis.daum.net/local/v1/search/keyword.json?" +
+                "apikey=" + daumApikey + "&query=" + searchString +
+                "&location=" + curloc + "&radius=" + radius +
+                "&sort=" + sort;
+
+        return requestUrl;
+
+
+        //0 : 정확도순
+        //1 : 인기순
+        //2 : 거리순
+
+        //https://apis.daum.net/local/v1/search/keyword.json?apikey={apikey}&query=카카오프렌즈
+        // &location=37.514322572335935,127.06283102249932&radius=20000
+    }
+
+
+    public static String createOpenAPIRequestURL(DATASOURCE datasource, double lat, double lon, int radius, int sort, String format, String daumApikey) {
         String requestUrl = "";
         return requestUrl;
     }
 
-    public static String createTourAPIRequestURL() {
-        String requestUrl = "";
+    public static String createTourAPIRequestURL(Locale locale) {
+
+        //  MobileApp 파라미터는 서비스(웹,앱 등)별로 활용 통계를 산출하기 위한 항목입니다. URL요청 시 반드시 기재 부탁드립니다.//====//== 파라미터 인코딩 예시(JSP 기준)
+        //  <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> //=== 서비스명이 영문인 경우 (인코딩 불필요)
+        //  String appName = “KoreaTourismOrganization”; //=== 서비스명이 한글(일문, 중문 등)인 경우 (인코딩 필수)
+        //  String appName = URLEncoder.encode(“한국관광공사”, "UTF-8"); http://api.visitkorea.or.kr/openapi/service/rest/EngService/areaCode?ServiceKey=ServiceKey&n
+        //  umOfRows=10&pageNo=1&MobileOS=AND&MobileApp=appName&_type=json
+
+        // EngService 영문
+        // JpnService 일문
+        // ChsService 중문간체
+        // ChtService 중문번체
+        // GerService 독어(독일어)
+        // FreService 불어(프랑스어)
+        // SpnService 서어(스페인어)
+        // RusService 노어(러시아어)
+
+        String appName = "Accommdations";
+        // TODO: 2016. 10. 2. locale 에따라서 바까야됨 일단은 영어로
+        String requestUrl = "http://api.visitkorea.or.kr/openapi/service/rest/EngService + ";
         return requestUrl;
     }
-
-
 
 
 
