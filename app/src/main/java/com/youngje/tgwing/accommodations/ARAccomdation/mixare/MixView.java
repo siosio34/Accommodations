ad58056400d26a1aac9595102039999b9566422a
young <siosio34@nate.com>
2016-12-12 05:29:11 +0900

    이미지 연동완료

--- app/src/main/java/com/youngje/tgwing/accommodations/ARAccomdation/mixare/MixView.java
+++ app/src/main/java/com/youngje/tgwing/accommodations/ARAccomdation/mixare/MixView.java
@@ -342,7 +342,7 @@ public class MixView extends Activity implements SensorEventListener, LocationLi
             if (intent.getAction().equals("NAVI")) {
                 String guide = intent.getStringExtra("GUIDE");
                 Snackbar.make(getWindow().getDecorView().getRootView(), guide, Snackbar.LENGTH_LONG)
-                        .setAction("네비게이션 종료", new View.OnClickListener() {
+                        .setAction("종료", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getApplicationContext(),"네비게이션을 종료합니다.",Toast.LENGTH_SHORT).show();
